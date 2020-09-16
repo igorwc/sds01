dd3604ca204e2eee914ebd5f41ef1b23440819ef
Igor Cavalcanti <igorwc@gmail.com>
2020-09-16 14:03:46 -0300

    created project

---
+++ backend/src/main/java/com/cavalcanti/search/GameSearchApplication.java
@@ -0,0 +1,13 @@
+package com.cavalcanti.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameSearchApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cavalcanti/search/GameSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavalcanti.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
