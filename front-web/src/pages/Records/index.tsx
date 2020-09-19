7ec4cc4fae470a809c1227ff3ee71cae890d861c
Igor Cavalcanti <igorwc@gmail.com>
2020-09-18 21:29:43 -0300

    Criando navegação com rotas

---
+++ front-web/src/pages/Records/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.css'
+import { ReactComponent as ArrowIcon } from '../../assets/arrow.svg'
+import { ReactComponent as GamerImage } from '../../assets/gamer.svg'
+
+
+const Records = () => (
+
+
+
+    <h1>Records</h1>
+)
+
+export default Records;
